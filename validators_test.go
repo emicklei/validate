3e3183a3693fb28332e28680522fb00cfc83074a
Ernest Micklei <emicklei@Ernests-iMac.local>
2015-03-24 22:59:00 +0100

    rename examples

--- validators_test.go
+++ validators_test.go
@@ -17,7 +17,7 @@ func TestPositiveInt(t *testing.T) {
 	}
 }
 
-func ExampleIsPositive() {
+func ExampleIntVarIsPositive() {
 	result := Int("length", -1).IsPositive("%v must be positive, got %v")
 	if result.IsError() {
 		log.Println(result.Message())
@@ -35,7 +35,7 @@ func TestBetweenInt(t *testing.T) {
 	}
 }
 
-func ExampleIsBetween() {
+func ExampleIntVarIsBetween() {
 	result := Int("grade", 12).IsBetween(1, 10, "%v must be between %v and %v, got %v")
 	if result.IsError() {
 		log.Println(result.Message())
@@ -61,6 +61,14 @@ func TestStringHasLength(t *testing.T) {
 	result := String("greeting", s).HasLengthBetween(6, 12, "%s length must be tween %v and %v, got %v")
 	if result.Message() != "greeting length must be tween 6 and 12, got 5" {
 		t.Errorf(result.Message())
+	}
+}
+
+func ExampleStringVarHasLengthBetween() {
+	s := "hello"
+	result := String("greeting", s).HasLengthBetween(6, 12, "%s length must be tween %v and %v, got %v")
+	if result.IsError() {
+		log.Println(result.Message())
 	}
 }
 
